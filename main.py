53eb6bef0ec3cc39129131dc5da117b08eb7e2f6
nut10834 <std.67122420325@ubru.ac.th>
2026-03-03 23:54:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ from pokemon import create_app
 
 app = create_app()
 
-import os
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+if name == "main":
+    app.run(debug=True)
