4858f63abb9f96fc04e024bf6ace5cdcbb3790e0
nut10834 <std.67122420325@ubru.ac.th>
2026-03-03 21:05:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ app = create_app()
 
 import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
